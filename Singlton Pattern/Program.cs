d6c9f478a320c93fb37e98f6a9e2b68dbf5060a9
يارب <@DESKTOP-GHSHA8O>
2022-12-25 23:43:30 +0200

    Add project files.

---
+++ Singlton Pattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Singlton_Pattern
+{
+    class Person
+    {
+      private static bool flag =false;
+        private Person()
+        {
+        }
+         public void Speak()
+        {
+            Console.WriteLine("Hello World");
+        }
+        public static Person obj()
+        {
+            if (flag == false) {
+                flag = true;
+                return new Person();            
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+           
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = Person.obj();
+            p.Speak();
+            Person s = Person.obj();
+            s.Speak();
+
+
+
+
+        }
+    }
+}
